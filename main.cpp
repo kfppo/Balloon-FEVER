2600667bcade09a5c7c17066a8c07c5ab045fc33
Ping Lin <46828728+pinglin07@users.noreply.github.com>
2019-02-14 23:50:00 -0500

    Update comments

--- main.cpp
+++ main.cpp
@@ -13,24 +13,24 @@ int main(int argc, char* args[])
     }
     else
     {
-		//Load media
+	//Load media
         if (!loadMedia())
         {
             cout << "Load media files failed!" << endl;
         }
         else
         {
-			//Start from intro state
+	    //Start from intro state
             stateID = STATE_INTRO;
             currentState = new Intro();
 
-			//Balloon giver start from standing state
+	    //Balloon giver start from standing state
             currentAction = &standingState;
 
-			//Add a kitty at before start
+	    //Add a kitty before start
             kittyList.push_back(new Kitty());
 
-			//Run the state
+	    //Run the state
             while (stateID != STATE_EXIT)
             {
                 currentState->handle_events();
